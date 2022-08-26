57d5c046630bccaf4131d1b6a7da73fdf5da1c80
junbro <khm11904@gmail.com>
2022-08-26 11:27:57 +0900

    Design: change title style & add point

--- src/components/QuestionWindow/index.tsx
+++ src/components/QuestionWindow/index.tsx
@@ -1,12 +1,15 @@
 import { useRecoilValue } from 'recoil';
 import styled from 'styled-components';
+import { AiTwotoneStar } from 'react-icons/ai';
 
 import { useQuestionsQuery } from '../../hooks/useQuestionsQuery';
 import quizState from '../../recoil/quiz/atom';
 import { Props } from './type';
+import resultInfoState from '../../recoil/resultInfo/atom';
 
 function QuestionWindow({ onSelect, questionNum }: Props) {
   const quiz = useRecoilValue(quizState);
+  const resultInfo = useRecoilValue(resultInfoState);
   const { data } = useQuestionsQuery({
     select: onSelect,
     enabled: quiz.solvingState === 'new',
@@ -17,6 +20,10 @@ function QuestionWindow({ onSelect, questionNum }: Props) {
       <QuestionTitle>
         {quiz.solvingState === 'review' ? `리뷰: 문제${questionNum}번` : `문제 ${questionNum}번`}
       </QuestionTitle>
+      <PointWrapper>
+        <PointIcon />
+        <PointText>{resultInfo.correctNum}</PointText>
+      </PointWrapper>
       <QuestionView dangerouslySetInnerHTML={{ __html: `${data?.question}` }} />
     </Wrapper>
   );
@@ -27,12 +34,33 @@ const Wrapper = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: space-between;
+  position: relative;
   margin-top: 10px;
   height: 300px;
 `;
 
 const QuestionTitle = styled.span`
-  font-size: 25px;
+  font-size: 28px;
+  font-weight: 800;
+`;
+
+const PointWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  position: absolute;
+  top: 0;
+  right: 10px;
+  width: 50px;
+  font-size: 30px;
+`;
+
+const PointIcon = styled(AiTwotoneStar)`
+  color: ${(props) => props.theme.colors.yellow};
+`;
+
+const PointText = styled.span`
+  color: ${(props) => props.theme.colors.main};
 `;
 
 const QuestionView = styled.div`
