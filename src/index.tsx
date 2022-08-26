0abdcfd6c516f826d56b5f8d0982ca8c8c65a84c
junbro <khm11904@gmail.com>
2022-08-26 17:15:54 +0900

    Chore: remove ReactQueryDevtools

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 
@@ -24,7 +23,6 @@ root.render(
           <App />
         </Router>
       </RecoilRoot>
-      <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>,
 );
