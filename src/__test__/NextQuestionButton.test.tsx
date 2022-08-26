7cb6ce57159c70e1c798cbfa4460c27cf3ee6c90
junbro <khm11904@gmail.com>
2022-08-26 12:49:52 +0900

    Test: update NextQuestionButton, QuestionWindow componet test

--- src/__test__/NextQuestionButton.test.tsx
+++ src/__test__/NextQuestionButton.test.tsx
@@ -1,5 +1,5 @@
 import NextQuestionButton from '../components/NextQuestionButton';
-import { render, screen } from '../setupTests';
+import { render, screen, fireEvent } from '../setupTests';
 
 describe('<NextQuestionButton/>', () => {
   it('If it is not the last question, the next question button is rendered.', () => {
@@ -22,5 +22,29 @@ describe('<NextQuestionButton/>', () => {
     const buttonText = screen.getByText('종료');
 
     expect(buttonText).toBeInTheDocument();
+  });
+
+  it('When the button is clicked, the event callback function is executed.', () => {
+    const handleButtonClick = jest.fn();
+    render(
+      <NextQuestionButton onNextButtonClick={handleButtonClick} isVisible={true} isLast={true} />,
+    );
+
+    const buttonText = screen.getByText('종료');
+
+    fireEvent.click(buttonText);
+
+    expect(handleButtonClick).toHaveBeenCalledTimes(1);
+  });
+
+  it('Buttons are not visible when the problem is not solved.', () => {
+    const handleButtonClick = jest.fn();
+    render(
+      <NextQuestionButton onNextButtonClick={handleButtonClick} isVisible={false} isLast={false} />,
+    );
+
+    const buttonText = screen.queryByText('다음 문항');
+
+    expect(buttonText).not.toBeInTheDocument();
   });
 });
--- src/__test__/QuestionWindow.test.tsx
+++ src/__test__/QuestionWindow.test.tsx
@@ -1,3 +1,4 @@
+import { Question } from '../api/type';
 import QuestionWindow from '../components/QuestionWindow';
 import { render, screen } from '../setupTests';
 
@@ -7,6 +8,15 @@ describe('<QuestionWindow/>', () => {
     render(<QuestionWindow onSelect={handleSelect} questionNum={1} />);
 
     const questionTitle = screen.getByText(/문제/i);
+
+    expect(questionTitle).toBeInTheDocument();
+  });
+
+  it('Question Num successfully rendered', () => {
+    const handleSelect = (data: Question[]) => data[1];
+    render(<QuestionWindow onSelect={handleSelect} questionNum={2} />);
+
+    const questionTitle = screen.getByText(/2/i);
 
     expect(questionTitle).toBeInTheDocument();
   });
